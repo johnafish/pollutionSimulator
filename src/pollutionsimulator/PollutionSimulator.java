c252519466f988d66a2b0479899670cb2269bf97
John Fish <john@johnafish.ca>
2015-10-09 12:04:29 -0400

    :heart: inital values set randomly

--- src/pollutionsimulator/PollutionSimulator.java
+++ src/pollutionsimulator/PollutionSimulator.java
@@ -1,5 +1,7 @@
 package pollutionsimulator;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 /**
  * @author John Fish <john@johnafish.ca>
  */
@@ -8,12 +10,13 @@ public class PollutionSimulator {
     int width = 400;
     int height = 400;
     double[][] peopleValues = new double[width][height];
-    double[][] atmosphericValue = new double[width][height];
+    double[][] atmosphericValues = new double[width][height];
     
     public void setInitial(){
         for (int i = 0; i < width; i++) {
             for (int j = 0; j < height; j++) {
-                
+                peopleValues[i][j] = ThreadLocalRandom.current().nextDouble(0.5, 1.5);
+                atmosphericValues[i][j] = 1;
             }
         }
     }
