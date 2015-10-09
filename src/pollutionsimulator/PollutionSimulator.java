c9304555771e491379de7db9bc9f11ab65f25ee1
John Fish <john@johnafish.ca>
2015-10-09 09:44:46 -0400

    Class Ended

--- src/pollutionsimulator/PollutionSimulator.java
+++ src/pollutionsimulator/PollutionSimulator.java
@@ -5,7 +5,19 @@ package pollutionsimulator;
  */
 
 public class PollutionSimulator {
-
+    int width = 400;
+    int height = 400;
+    double[][] peopleValues = new double[width][height];
+    double[][] atmosphericValue = new double[width][height];
+    
+    public void setInitial(){
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                
+            }
+        }
+    }
+    
     public static void main(String[] args) {
 
     }
